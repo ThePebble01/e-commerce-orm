4a99ab699be7e2367f2230dc25c4f1fea139ca5c
unknown <john.gordos@outlook.com>
2023-06-26 09:43:16 -0600

    fixed var declaration in .env (from process.env.DB_NAME to DB_NAME)

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const Sequelize = require("sequelize");
-//new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+console.log(process.env);
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("ecommerce_db", "root", "Rootmysql1!", {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
       dialectOptions: {
