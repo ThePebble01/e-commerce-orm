efeebf3f9ec7d5fcabf6d97efe5a4418ce70eac0
unknown <john.gordos@outlook.com>
2023-06-26 09:21:46 -0600

    finished defining the objects and associations

--- models/index.js
+++ models/index.js
@@ -1,16 +1,23 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
